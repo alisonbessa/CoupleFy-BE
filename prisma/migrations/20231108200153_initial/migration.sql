3f7a1ebc1482acd138e50a5778e71955c1a18bab
Alison Bessa <alisonbessa@gmail.com>
2023-11-08 17:31:22 -0300

    feat: minor adjustments

--- prisma/migrations/20231108200153_initial/migration.sql
+++ prisma/migrations/20231108200153_initial/migration.sql
@@ -1,4 +1,26 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "costCenterId" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CostCenter" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CostCenter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
@@ -14,6 +36,12 @@ CREATE TABLE "Transaction" (
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_costCenterId_fkey" FOREIGN KEY ("costCenterId") REFERENCES "CostCenter"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,168 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  // Clear database
+  await prisma.$executeRaw`TRUNCATE TABLE "User" CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CostCenter" CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Transaction" CASCADE;`;
+
+  console.log('database cleared');
+
+  // Create 4 dummy users
+  const users = await Promise.all([
+    prisma.user.upsert({
+      where: { email: 'user1@example.com' },
+      update: {},
+      create: {
+        name: 'User 1',
+        email: 'user1@example.com',
+        password: 'password1',
+        costCenterId: null,
+      },
+    }),
+    prisma.user.upsert({
+      where: { email: 'user2@example.com' },
+      update: {},
+      create: {
+        name: 'User 2',
+        email: 'user2@example.com',
+        password: 'password2',
+        costCenterId: null,
+      },
+    }),
+    prisma.user.upsert({
+      where: { email: 'user3@example.com' },
+      update: {},
+      create: {
+        name: 'User 3',
+        email: 'user3@example.com',
+        password: 'password3',
+        costCenterId: null,
+      },
+    }),
+    prisma.user.upsert({
+      where: { email: 'user4@example.com' },
+      update: {},
+      create: {
+        name: 'User 4',
+        email: 'user4@example.com',
+        password: 'password4',
+        costCenterId: null,
+      },
+    }),
+  ]);
+
+  // Create 2 dummy cost centers
+  const costCenters = await Promise.all([
+    prisma.costCenter.create({
+      data: {},
+    }),
+    prisma.costCenter.create({
+      data: {},
+    }),
+  ]);
+
+  // Associate 2 users with each cost center
+  await Promise.all([
+    prisma.user.update({
+      where: {
+        id: users[0].id,
+      },
+      data: {
+        costCenterId: costCenters[0].id,
+      },
+    }),
+    prisma.user.update({
+      where: {
+        id: users[1].id,
+      },
+      data: {
+        costCenterId: costCenters[0].id,
+      },
+    }),
+    prisma.user.update({
+      where: {
+        id: users[2].id,
+      },
+      data: {
+        costCenterId: costCenters[1].id,
+      },
+    }),
+    prisma.user.update({
+      where: {
+        id: users[3].id,
+      },
+      data: {
+        costCenterId: costCenters[1].id,
+      },
+    }),
+  ]);
+
+  // Create dummy transactions for the users
+  const transactions = await Promise.all([
+    prisma.transaction.create({
+      data: {
+        title: 'Transaction 1',
+        amount: 100.0,
+        category: 'Groceries',
+        subcategory: 'Food',
+        paymentMethod: 'Credit Card',
+        authorId: users[0].id,
+        costCenterId: costCenters[0].id,
+      },
+    }),
+    prisma.transaction.create({
+      data: {
+        title: 'Transaction 2',
+        amount: 50.0,
+        category: 'Entertainment',
+        subcategory: 'Movies',
+        paymentMethod: 'Debit Card',
+        authorId: users[1].id,
+        costCenterId: costCenters[0].id,
+      },
+    }),
+    prisma.transaction.create({
+      data: {
+        title: 'Transaction 3',
+        amount: 75.0,
+        category: 'Shopping',
+        subcategory: 'Clothing',
+        paymentMethod: 'Cash',
+        authorId: users[2].id,
+        costCenterId: costCenters[1].id,
+      },
+    }),
+    prisma.transaction.create({
+      data: {
+        title: 'Transaction 4',
+        amount: 120.0,
+        category: 'Dining',
+        subcategory: 'Restaurant',
+        paymentMethod: 'Credit Card',
+        authorId: users[3].id,
+        costCenterId: costCenters[1].id,
+      },
+    }),
+  ]);
+
+  console.log('Dummy data created successfully:', {
+    users,
+    costCenters,
+    transactions,
+  });
+}
+
+// Call the function to create dummy data
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
